b3d8bf45a71420eb4cba2cd96093d4f5dbcfa2ba
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2025-01-21 23:30:38 -0300

    Mudanças no react router

--- src/main.tsx
+++ src/main.tsx
@@ -16,12 +16,12 @@ createRoot(document.getElementById('root')!).render(
         <GettingMoviesProvider>
         <Routes>
             <Route element={<App/>}>
-                <Route exact path="/" element={<Home/>} />
-                <Route exact path="/nowplayingmovies" element={<AllMovies/>}/>
-                <Route exact path="/movie/:id" element={<Movie/>}></Route>
-                <Route exact path="/search" element={<Search/>}></Route>
-                <Route exact path="/nowplayingseries" element={<AllSeries/>}/>
-                <Route exact path="/tv/:id" element={<Serie/>}></Route>
+                <Route path="/" element={<Home/>} />
+                <Route path="/nowplayingmovies" element={<AllMovies/>}/>
+                <Route path="/movie/:id" element={<Movie/>}></Route>
+                <Route path="/search" element={<Search/>}></Route>
+                <Route path="/nowplayingseries" element={<AllSeries/>}/>
+                <Route path="/tv/:id" element={<Serie/>}></Route>
             </Route>
         </Routes>
         </GettingMoviesProvider>
