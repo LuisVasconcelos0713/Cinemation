3bca3984cb77c1239fea639e323ad67eb5a46400
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2025-01-22 00:10:34 -0300

    alterações de tipagem

--- src/Pages/Movie/Movie.tsx
+++ src/Pages/Movie/Movie.tsx
@@ -2,62 +2,77 @@ import "../../App.css"
 import {useParams} from "react-router-dom";
 import {useEffect, useState} from "react";
 import {FaStar} from "react-icons/fa";
-import {useMovies} from "../../Usable/GettingMoviesProvider.tsx";
 
 const movieUrl = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 const imgUrl = import.meta.env.VITE_IMG;
 
+interface IMovieProvider {
+    provider_name: string;
+    logo_path: string;
+}
+
+interface IMovieCast {
+    name: string;
+}
+
+interface IMovieDirector {
+    name: string;
+}
+
+interface IMovieData {
+    title: string;
+    vote_average: number;
+    release_date: string;
+    runtime: number;
+    genres: { name: string }[];
+    overview: string;
+    poster_path: string;
+}
+
 const Movie = () => {
-    {/* States and Params*/}
-    const [movie, setMovie] = useState(null)
-    const {id} = useParams()
-    const [providersStream, setProvidersStream] = useState([])
-    const [providersRent, setProvidersRent] = useState([])
-    const [cast,setCast] = useState([])
-    const [director, setDirector] = useState([])
+    const [movie, setMovie] = useState<IMovieData | null>(null);
+    const {id} = useParams();
+    const [providersStream, setProvidersStream] = useState<IMovieProvider[]>([]);
+    const [providersRent, setProvidersRent] = useState<IMovieProvider[]>([]);
+    const [cast, setCast] = useState<IMovieCast[]>([]);
+    const [director, setDirector] = useState<IMovieDirector[]>([]);
 
-    {/* getting apis*/}
-    const getMovie = async (url) => {
-        const res = await fetch(url)
-        const data = await res.json()
-        setMovie(data)
-    }
-
-    const getProviders = async (url) => {
-        const res = await fetch(url)
-        const data = await res.json();
-        // Verifique se BR e flatrate/rent existem antes de setar o estado
-        setProvidersStream(data.results.BR?.flatrate || []);  // Se não houver flatrate, use um array vazio
-        setProvidersRent(data.results.BR?.rent || []);  // Se não houver rent, use um array vazio
-    }
-
-    const getCast = async (url) => {
+    const getMovie = async (url: string) => {
         const res = await fetch(url);
         const data = await res.json();
+        setMovie(data);
+    };
 
-        const actors = data.cast.slice(0,4)
-        const directorsList = data.crew.filter((crew) => crew.job === "Director")
+    const getProviders = async (url: string) => {
+        const res = await fetch(url);
+        const data = await res.json();
+        setProvidersStream(data.results.BR?.flatrate || []);
+        setProvidersRent(data.results.BR?.rent || []);
+    };
 
-        setCast(actors)
-        setDirector(directorsList)
-
-    }
+    const getCast = async (url: string) => {
+        const res = await fetch(url);
+        const data = await res.json();
+        const actors = data.cast.slice(0, 4);
+        const directorsList = data.crew.filter((crew) => crew.job === "Director");
+        setCast(actors);
+        setDirector(directorsList);
+    };
 
     useEffect(() => {
-        getMovie(`${movieUrl}${id}?${apiKey}&language=pt-br`)
-        const urlProvider = `${movieUrl}${id}/watch/providers?${apiKey}`
-        const urlCast = `${movieUrl}${id}/credits?${apiKey}`
-        getProviders(urlProvider)
-        getCast(urlCast)
+        getMovie(`${movieUrl}${id}?${apiKey}&language=pt-br`);
+        const urlProvider = `${movieUrl}${id}/watch/providers?${apiKey}`;
+        const urlCast = `${movieUrl}${id}/credits?${apiKey}`;
+        getProviders(urlProvider);
+        getCast(urlCast);
     }, [id]);
 
-    {/* utility functions */}
-    const convertRuntime = (runtime) => {
-        const hours = Math.round(runtime/60)
-        const minutes = (runtime%60).toFixed(0)
-        return `${hours}h${minutes}min`
-    }
+    const convertRuntime = (runtime: number): string => {
+        const hours: number = Math.round(runtime / 60);
+        const minutes: string = (runtime % 60).toFixed(0);
+        return `${hours}h${minutes}min`;
+    };
 
     return (
         <div>
@@ -74,7 +89,7 @@ const Movie = () => {
                         <div className="flex md:flex-row items-center justify-between max-w-[900px] phone:flex-col">
                             <h1 className="text-white font-bold md:text-5xl phone:text-3xl">{movie.title}</h1>
                             <p className="flex flex-row items-center gap-1 text-WhiteText text-3xl">
-                                <FaStar className="text-yellow-400"/> {movie.vote_average.toFixed(1)}
+                                <FaStar className="text-yellow-400" /> {movie.vote_average.toFixed(1)}
                             </p>
                         </div>
 
@@ -85,45 +100,41 @@ const Movie = () => {
                             <span>|</span>
                             <p className="time">{convertRuntime(movie.runtime)}</p>
                             <span>|</span>
-                            <p className="genre">{movie.genres
-                                .map((genre) => genre.name)
-                                .join(", ")}</p>
+                            <p className="genre">{movie.genres.map((genre) => genre.name).join(", ")}</p>
                         </div>
 
                         <div className="flex flex-col mt-12 md:max-w-[800px] phone:max-w-[375px]">
                             <h1 className="text-white font-bold text-xl">OVERVIEW</h1>
                             <p className="text-WhiteText text-lg mt-4">{movie.overview}</p>
                             <div className="flex flex-col mt-9">
-                                <p className="text-Gray4 flex flex-row md:gap-3 md:text-[18px] gap-3 phone:text-[11.5px]">Starring: {cast.map((cast) => <h1
-                                    className="text-WhiteText ">{cast.name}</h1>)}</p>
+                                <p className="text-Gray4 flex flex-row md:gap-3 md:text-[18px] gap-3 phone:text-[11.5px]">
+                                    Starring: {cast.map((actor) => <h1 key={actor.name} className="text-WhiteText">{actor.name}</h1>)}
+                                </p>
 
-                                <p className="text-Gray4 mt-3 flex flex-row md:gap-3 md:text-[18px] gap-3 phone:text-[13px]">Created by
-                                    : {director.map((director) => <h1
-                                        className="text-WhiteText">{director.name}</h1>)}</p>
+                                <p className="text-Gray4 mt-3 flex flex-row md:gap-3 md:text-[18px] gap-3 phone:text-[13px]">
+                                    Created by: {director.map((director) => <h1 key={director.name} className="text-WhiteText">{director.name}</h1>)}
+                                </p>
 
-                                <p className="text-Gray4 mt-3 flex flex-row md:text-[18px] gap-3 phone:text-[13px]">Genre :
-                                    <h1 className="text-WhiteText">{movie.genres
-                                        .map((genre) => genre.name)
-                                        .join(", ")}</h1>
+                                <p className="text-Gray4 mt-3 flex flex-row md:text-[18px] gap-3 phone:text-[13px]">
+                                    Genre:
+                                    <h1 className="text-WhiteText">{movie.genres.map((genre) => genre.name).join(", ")}</h1>
                                 </p>
                             </div>
                         </div>
                         <h1 className="text-WhiteText mt-6 font-bold text-lg">Stream</h1>
-                        <div className="flex flex-row items-start ">
+                        <div className="flex flex-row items-start">
                             {providersStream.map((provider, index) => (
                                 <div className="flex flex-col items-center mr-3" key={index}>
-                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`}
-                                         className="w-[54px]"/>
-                                    <h1 className="text-WhiteText text-xs]">{provider.provider_name}</h1>
+                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`} className="w-[54px]" />
+                                    <h1 className="text-WhiteText text-xs">{provider.provider_name}</h1>
                                 </div>
                             ))}
                         </div>
                         <h1 className="text-WhiteText mt-2 font-bold text-lg">Rent</h1>
-                        <div className="flex flex-row items-start ">
+                        <div className="flex flex-row items-start">
                             {providersRent.map((provider, index) => (
                                 <div className="flex flex-col items-center mr-3" key={index}>
-                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`}
-                                         className="w-[54px]"/>
+                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`} className="w-[54px]" />
                                     <h1 className="text-WhiteText text-xs">{provider.provider_name}</h1>
                                 </div>
                             ))}
@@ -134,8 +145,7 @@ const Movie = () => {
                 <p>Carregando...</p>
             )}
         </div>
-    )
-
-}
+    );
+};
 
 export default Movie;
--- src/Pages/Serie/Serie.tsx
+++ src/Pages/Serie/Serie.tsx
@@ -7,58 +7,103 @@ const serieUrl = import.meta.env.VITE_SERIES
 const apiKey = import.meta.env.VITE_API_KEY;
 const imgUrl = import.meta.env.VITE_IMG;
 
+interface IGenre {
+    id: number;
+    name: string;
+}
+
+interface IProvider {
+    provider_name: string;
+    logo_path: string;
+}
+
+interface ICast {
+    id: number;
+    name: string;
+    character: string;
+    profile_path: string | null;
+}
+
+interface IDirector {
+    id: number;
+    name: string;
+    job: string;
+}
+
+interface ISerieData {
+    id: number;
+    name: string;
+    first_air_date: string;
+    number_of_seasons: number;
+    number_of_episodes: number;
+    genres: IGenre[];
+    overview: string;
+    poster_path: string;
+    vote_average: number;
+}
+
+interface ISerieProviderResponse {
+    results: {
+        BR: {
+            flatrate: IProvider[];
+            rent: IProvider[];
+        };
+    };
+}
+
+interface ISerieCastResponse {
+    cast: ICast[];
+    crew: IDirector[];
+}
+
 const Serie = () => {
-    {/* States and Params*/}
-    const [serie, setserie] = useState(null)
-    const {id} = useParams()
-    const [providersStream, setProvidersStream] = useState([])
-    const [providersRent, setProvidersRent] = useState([])
-    const [cast,setCast] = useState([])
-    const [director, setDirector] = useState([])
+    // Definindo os estados com suas respectivas tipagens
+    const [serie, setSerie] = useState<ISerieData | null>(null);
+    const { id } = useParams<{ id: string }>(); // Tipagem para os parâmetros da URL
+    const [providersStream, setProvidersStream] = useState<IProvider[]>([]);
+    const [providersRent, setProvidersRent] = useState<IProvider[]>([]);
+    const [cast, setCast] = useState<ICast[]>([]);
+    const [director, setDirector] = useState<IDirector[]>([]);
 
-    {/* getting apis*/}
-    const getserie = async (url) => {
-        const res = await fetch(url)
-        const data = await res.json()
-        setserie(data)
-    }
-
-    const getProviders = async (url) => {
-        const res = await fetch(url)
-        const data = await res.json();
-        // Verifique se BR e flatrate/rent existem antes de setar o estado
-        setProvidersStream(data.results.BR?.flatrate || []);  // Se não houver flatrate, use um array vazio
-        setProvidersRent(data.results.BR?.rent || []);  // Se não houver rent, use um array vazio
-    }
-
-    const getCast = async (url) => {
+    // Função para obter dados da série
+    const getSerie = async (url: string) => {
         const res = await fetch(url);
-        const data = await res.json();
+        const data: ISerieData = await res.json();
+        setSerie(data);
+    };
 
-        const actors = data.cast.slice(0,4)
-        const directorsList = data.crew.filter((crew) => crew.known_for_department === "Directing")
+    // Função para obter provedores
+    const getProviders = async (url: string) => {
+        const res = await fetch(url);
+        const data: ISerieProviderResponse = await res.json();
+        setProvidersStream(data.results.BR?.flatrate || []);
+        setProvidersRent(data.results.BR?.rent || []);
+    };
 
-        setCast(actors)
-        setDirector(directorsList)
+    // Função para obter elenco e diretor
+    const getCast = async (url: string) => {
+        const res = await fetch(url);
+        const data: ISerieCastResponse = await res.json();
 
-    }
+        const actors = data.cast.slice(0, 4);
+        const directorsList = data.crew.filter(
+            (crew) => crew.job === "Director"
+        );
 
+        setCast(actors);
+        setDirector(directorsList);
+    };
+
+    // Efeito para carregar os dados assim que o ID da série mudar
     useEffect(() => {
-        getserie(`${serieUrl}${id}?${apiKey}&language=pt-br`)
-        const urlProvider = `${serieUrl}${id}/watch/providers?${apiKey}`
-        const urlCast = `${serieUrl}${id}/credits?${apiKey}`
-        getProviders(urlProvider)
-        getCast(urlCast)
-        console.log(urlCast)
-        console.log(urlProvider)
-        console.log(urlCast)
+        if (id) {
+            getSerie(`${serieUrl}${id}?${apiKey}&language=pt-br`);
+            const urlProvider = `${serieUrl}${id}/watch/providers?${apiKey}`;
+            const urlCast = `${serieUrl}${id}/credits?${apiKey}`;
+            getProviders(urlProvider);
+            getCast(urlCast);
+        }
     }, [id]);
-
-    {/* utility functions */}
-
-    console.log(director)
-
-    console.log(cast)
 
     return (
         <div>
@@ -75,14 +120,12 @@ const Serie = () => {
                         <div className="flex md:flex-row items-center justify-between max-w-[900px] phone:flex-col">
                             <h1 className="text-white font-bold text-5xl">{serie.name}</h1>
                             <p className="flex flex-row items-center gap-1 text-WhiteText text-3xl">
-                                <FaStar className="text-yellow-400"/> {serie.vote_average.toFixed(1)}
+                                <FaStar className="text-yellow-400" /> {serie.vote_average.toFixed(1)}
                             </p>
                         </div>
 
                         <div className="flex flex-row items-center gap-3 mt-1 md:justify-start text-Gray4 md:text-[18px] phone:text-[17px] phone:justify-center">
-                            <p className="serie-release">
-                                {serie.first_air_date.substring(0, 4)}
-                            </p>
+                            <p className="serie-release">{serie.first_air_date.substring(0, 4)}</p>
                             <span>|</span>
                             <p className="time">{`${serie.number_of_seasons} Seasons`}</p>
                             <span>|</span>
@@ -93,36 +136,33 @@ const Serie = () => {
                             <h1 className="text-white font-bold text-xl">OVERVIEW</h1>
                             <p className="text-WhiteText text-lg mt-4">{serie.overview}</p>
                             <div className="flex flex-col mt-9">
-                                <p className="text-Gray4 flex flex-row md:text-[18px] gap-3 phone:text-[11.5px]">Starring : {cast.map((cast) => <h1
-                                    className="text-WhiteText">{cast.name}</h1>)}</p>
+                                <p className="text-Gray4 flex flex-row md:text-[18px] gap-3 phone:text-[11.5px]">
+                                    Starring : {cast.map((cast) => <h1 className="text-WhiteText">{cast.name}</h1>)}
+                                </p>
 
-                                <p className="text-Gray4 mt-3 flex flex-row md:text-[18px] gap-3 phone:text-[11.5px]">Created by
-                                    : {director.map((director) => <h1
-                                        className="text-WhiteText">{director.name}</h1>)}</p>
+                                <p className="text-Gray4 mt-3 flex flex-row md:text-[18px] gap-3 phone:text-[11.5px]">
+                                    Created by : {director.map((director) => <h1 className="text-WhiteText">{director.name}</h1>)}
+                                </p>
 
-                                <p className="text-Gray4 mt-3 flex flex-row md:text-[18px] gap-3 phone:text-[13px]">Genre :
-                                    <h1 className="text-WhiteText">{serie.genres
-                                        .map((genre) => genre.name)
-                                        .join(", ")}</h1>
+                                <p className="text-Gray4 mt-3 flex flex-row md:text-[18px] gap-3 phone:text-[13px]">
+                                    Genre : <h1 className="text-WhiteText">{serie.genres.map((genre) => genre.name).join(", ")}</h1>
                                 </p>
                             </div>
                         </div>
                         <h1 className="text-WhiteText mt-6 font-bold text-lg">Stream</h1>
-                        <div className="flex flex-row items-start ">
-                            {providersStream.map((provider) => (
-                                <div className="flex flex-col items-center mr-3">
-                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`}
-                                         className="w-[54px]"/>
-                                    <h1 className="text-WhiteText text-xs]">{provider.provider_name}</h1>
+                        <div className="flex flex-row items-start">
+                            {providersStream.map((provider, index) => (
+                                <div className="flex flex-col items-center mr-3" key={index}>
+                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`} className="w-[54px]" />
+                                    <h1 className="text-WhiteText text-xs">{provider.provider_name}</h1>
                                 </div>
                             ))}
                         </div>
                         <h1 className="text-WhiteText mt-2 font-bold text-lg">Rent</h1>
-                        <div className="flex flex-row items-start ">
-                            {providersRent.map((provider) => (
-                                <div className="flex flex-col items-center mr-3">
-                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`}
-                                         className="w-[54px]"/>
+                        <div className="flex flex-row items-start">
+                            {providersRent.map((provider, index) => (
+                                <div className="flex flex-col items-center mr-3" key={index}>
+                                    <img src={imgUrl + provider.logo_path} alt={`${provider.provider_name} logo`} className="w-[54px]" />
                                     <h1 className="text-WhiteText text-xs">{provider.provider_name}</h1>
                                 </div>
                             ))}
@@ -133,8 +173,7 @@ const Serie = () => {
                 <p>Carregando...</p>
             )}
         </div>
-    )
-
-}
+    );
+};
 
 export default Serie;
